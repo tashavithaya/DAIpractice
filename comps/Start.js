2fa30aa8100f87ba69941197eda8d3c73db875b4
tashavithaya <tashavithaya@gmail.com>
2019-10-09 09:35:32 -0700

    added Start

---
+++ comps/Start.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import {View, Text, Image} from 'react-native';
+
+function Start(){
+    return (
+        <View>
+            <Text>Welcome to Grilled Cheese</Text>
+            <Image source={{url: "https://bit.ly/2q1fpXF"}}/>
+        </View>
+    )
+}
+
+export default Start;
