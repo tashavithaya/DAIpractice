acdc6122f17879b7263a5a8f93c23e7b13269de3
tashavithaya <tashavithaya@gmail.com>
2019-10-09 09:52:17 -0700

    added StartStyle

---
+++ styles/StartStyles.js
@@ -0,0 +1,14 @@
+import {StyleSheet} from 'react-native';
+
+var StartStyles = StyleSheet.create({
+    app:{
+        flex:1,
+        justifyContent:"center",
+        alignItems:"center",
+        width:200,
+        height:200
+        
+    }
+})
+
+export default StartStyles;
