2c813354028988ca363f0eaf097d62b269193234
DeriqDanois <deriqdanois@gmail.com>
2019-10-09 10:18:15 -0700

    appjs change

--- App.js
+++ App.js
@@ -11,32 +11,5 @@ function App(){
     </View>
   )
 }
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow
- */
 
-import React from 'react';
-import {
-  SafeAreaView,
-  StyleSheet,
-  ScrollView,
-  View,
-  Text,
-  StatusBar,
-} from 'react-native';
-import Start from './comps/Start';
-
-function App(){
-    return(
-    <View>
-      <Start />
-    </View>
-    )
-}
-
-
-export default App;
+export default App();
